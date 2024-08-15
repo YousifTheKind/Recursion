f13263d1cbd17fa84af36df59a5d799245825274
YousifTheKind <yousifmoeltayeb@gmail.com>
2024-08-15 21:06:19 +0300

    Modify merge sort solution

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -1,18 +1,72 @@
 #!/usr/bin/env node
 
-const mergeThis = [8, 7, 0, 1];
-function mergeSort(arr) {
-    if (arr.length < 2) return arr;
+const mergeThis = [3, 2, 1, 13, 8, 5, 0, 1];
+const mergeSort = (arr) => {
+    if (arr.length === 1) return arr;
 
-    let halfLength = Math.ceil(arr.length / 2);
+    const halfLength = Math.floor(arr.length / 2);
     let left = arr.slice(0, halfLength);
     let right = arr.slice(halfLength, arr.length);
 
-    mergeSort(left);
-    mergeSort(right);
-    let mergedArr = left.concat(right);
-    return mergedArr.toSorted(function (a, b) {
-        return a - b;
-    });
-}
+    return merge(mergeSort(left), mergeSort(right));
+};
+
+// Using shift()
+// const merge = (left, right) => {
+//     const mergedArr = [];
+//     console.log("LEFT: " + left);
+//     console.log("RIGHT: " + right);
+
+//     while (0 < left.length && 0 < right.length) {
+//         if (left[0] < right[0]) {
+//             mergedArr.push(left[0]);
+//             left.shift();
+//         } else {
+//             mergedArr.push(right[0]);
+//             right.shift();
+//         }
+//     }
+//     while (0 < left.length) {
+//         mergedArr.push(left[0]);
+//         left.shift();
+//     }
+
+//     while (0 < right.length) {
+//         mergedArr.push(right[0]);
+//         right.shift();
+//     }
+
+//     console.log("MERGED: " + mergedArr);
+
+//     return mergedArr;
+// };
+
+// using indexes
+const merge = (leftArr, rightArr) => {
+    const mergedArr = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        if (leftArr[leftIndex] < rightArr[rightIndex]) {
+            mergedArr.push(leftArr[leftIndex]);
+            leftIndex++;
+        } else {
+            mergedArr.push(rightArr[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    while (leftIndex < leftArr.length) {
+        mergedArr.push(leftArr[leftIndex]);
+        leftIndex++;
+    }
+
+    while (rightIndex < rightArr.length) {
+        mergedArr.push(rightArr[rightIndex]);
+        rightIndex++;
+    }
+
+    return mergedArr;
+};
 console.log(mergeSort(mergeThis));
